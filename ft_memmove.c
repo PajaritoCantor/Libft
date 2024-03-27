e10b93ad8aba89f18adbbb3c2956314225cae1c0
PajaritoCantor <157131868+PajaritoCantor@users.noreply.github.com>
2024-03-27 15:51:48 +0100

    mains %s y %d / explicación

--- ft_memmove.c
+++ ft_memmove.c
@@ -55,19 +55,18 @@ int main()
 	return 0;
 }
 //arrays
-*/
 
 - La función memmove se utiliza para copiar n bytes de src a dst. 
-	* Si __src__ y **dst** se superponen, la función memmove garantiza que los datos originales en src se copien correctamente a dst.
-- void *dst, const void *src, size_t len: Son los parámetros de la función
+	* Si src y dst se superponen, la función memmove garantiza que los datos originales en src se copien correctamente a dst.
+- void *dst, const void *src, size_t len, son los parámetros de la función
 	* dst es el puntero al destino donde quieres copiar los datos
 	* src es el puntero a la fuente de donde quieres copiar los datoss
 	* len es la cantidad de bytes que quieres copiar
-- unsigned char *d, *s Estos son punteros a los datos de destino y origen, respectivamente, y se utilizan para acceder a los datos byte a byte.
-- if (!dst && !src) return (NULL); Este es un chequeo de seguridad para asegurarse de que tanto src como dst no son NULL.
+- unsigned char *d y unsigned char *s son punteros a los datos de destino y origen, respectivamente, y se utilizan para acceder a los datos byte a byte.
+- if (!dst && !src) return (NULL); es un chequeo de seguridad para asegurarse de que tanto src como dst no son NULL.
 	* Si alguno de ellos es NULL, la función retorna NULL
--if (s < d && d < s + len) Este chequeo se realiza para ver si la memoria de origen y destino se superpone.
-	* Si se superponen y el origen está antes que el destino, entonces copia los datos de atrás hacia adelante para evitar la sobreescritura de los datos que aún no se han copiado.
-- while (len--) *(d++) = *(s++); Este es el caso cuando la memoria de origen y destino no se superpone, o si se superpone pero el destino está antes que el origen.
+-if (s < d && d < s + len) es un chequeo que se realiza para ver si la memoria de origen y destino se superpone.
+	* Si se superpone y el origen está antes que el destino, entonces copia los datos de atrás hacia adelante para evitar la sobreescritura de los datos que aún no se han copiado.
+- while (len--) *(d++) = *(s++); es el caso cuando la memoria de origen y destino no se superpone, o sí se superpone pero el destino está antes que el origen.
 	* En este caso, se pueden copiar los datos de adelante hacia atrás.
 - return (dst); Finalmente, la función retorna un puntero al destino.
